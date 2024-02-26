c1d02e2cb695f67a58b2c07779c25fc501802d5e
Supragya Raj <supragyaraj@gmail.com>
2024-02-26 23:03:13 +0530

    init: basic univariate polyiop zerotest

---
+++ univariate-polynomial-iop-zerotest/README.md
@@ -0,0 +1,3 @@
+# Univariate Polynomial Interactive Oracle Proof for Zero Testing
+
+This sub-repo contains code for testing that a univariate polynomial $f(x)$ has evaluation $f(x) = 0$ for all $\Omega = \{{\omega, \omega^2, ..., \omega^{k-1}\}}$. The elements in $\Omega$ are assumed to be finite cyclic group in a prime field $F_p$ such that $\omega^k = 1 \mod{p}$. $\omega$ in this case would be called the $k$-th root of unity under $F_p$.
---
+++ univariate-polynomial-iop-zerotest/src/lib.rs
@@ -0,0 +1,26 @@
+use ark_ff::fields::{Fp64, MontBackend, MontConfig};
+
+/// 41 = (2^3 x 5) + 1
+/// Hence, a 8th root of unity would exist in this
+/// 3 is such root of unity. 3^8 = 6561. And 6561 % 41 = 1
+/// Building a field is given at: https://docs.rs/ark-ff/latest/ark_ff/fields/trait.Field.html
+#[derive(MontConfig)]
+#[modulus = "41"]
+#[generator = "2"]
+#[small_subgroup_base = "3"]
+#[small_subgroup_power = "8"]
+pub struct FqConfig;
+pub type Fq = Fp64<MontBackend<FqConfig, 1>>;
+
+#[cfg(test)]
+mod tests {
+    use ark_ff::{BigInt, Field, PrimeField};
+
+    use super::*;
+
+    #[test]
+    fn ensure_facts() {
+        assert_eq!(<Fq as PrimeField>::MODULUS, BigInt::<1>([41]));
+        assert_eq!(Fq::from(3).pow([8]), Fq::ONE);
+    }
+}
