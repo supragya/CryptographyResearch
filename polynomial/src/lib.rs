b6c19a34fb06bf396fc39942ff6a717ce8ef75dd
Supragya Raj <supragyaraj@gmail.com>
2024-03-07 00:45:24 +0800

    test: polynomial degree (#2)

--- polynomial/src/lib.rs
+++ polynomial/src/lib.rs
@@ -1,4 +1,9 @@
-use std::{fmt::Debug, ops::{Mul, Sub}};
+use std::{
+    collections::HashSet,
+    fmt::Debug,
+    hash::Hash,
+    ops::{Mul, Sub},
+};
 
 use num_traits::{One, Zero};
 
@@ -27,13 +32,30 @@ where
             repr: PolynomialRepr::Coeff(coeffs.to_vec()),
         }
     }
+}
 
+impl<T> Polynomial<T> {
+    /// Gets the degree of underlying polynomial
+    pub fn degree(&self) -> usize {
+        match &self.repr {
+            PolynomialRepr::Points(_points) => unimplemented!(),
+            PolynomialRepr::Roots(roots) => roots.len(),
+            PolynomialRepr::Coeff(coeffs) => coeffs.len(),
+        }
+    }
+}
+
+impl<T> Polynomial<T>
+where
+    T: Clone + Hash + Eq,
+{
     /// Generate a polynomial from its roots `r_0`, `r_1`, ... `r_n`
     /// when polynomial looks as follows:
     /// `(x - r_0)(x - r_1)(x - r_2)...(x - r_n)`
     pub fn new_from_roots(roots: &[T]) -> Self {
+        let distinct_roots = Vec::from_iter(HashSet::<T>::from_iter(roots.to_owned()));
         Self {
-            repr: PolynomialRepr::Roots(roots.to_vec()),
+            repr: PolynomialRepr::Roots(distinct_roots.to_vec()),
         }
     }
 }
@@ -60,7 +82,7 @@ impl<T: Zero + One + Mul<Output = T> + Sub<Output = T> + Clone + Debug> Polynomi
                     result = (x.clone() - n.clone()) * result;
                 }
                 result
-            },
+            }
             PolynomialRepr::Coeff(coeffs) => {
                 let mut result: T = Zero::zero();
                 for n in coeffs.iter().rev() {
@@ -90,5 +112,12 @@ mod tests {
         assert_eq!(polynomial.eval(1), 0);
         assert_eq!(polynomial.eval(2), 0);
         assert_eq!(polynomial.eval(3), 0);
+    }
+
+    #[test]
+    fn polynomial_degree() {
+        assert_eq!(Polynomial::<u32>::new_from_coeffs(&[3, 2, 1]).degree(), 3);
+        assert_eq!(Polynomial::<u32>::new_from_roots(&[3, 2, 1]).degree(), 3);
+        assert_eq!(Polynomial::<u32>::new_from_roots(&[3, 2, 3]).degree(), 2);
     }
 }
