7bfb246d64c8f8c014bc8b521254681f818e0376
Supragya Raj <supragyaraj@gmail.com>
2024-03-06 21:11:18 +0530

    add: basic poly

---
+++ polynomial/src/lib.rs
@@ -0,0 +1,70 @@
+use std::ops::Mul;
+
+use num_traits::Zero;
+
+pub enum PolynomialRepr<T> {
+    Points(Vec<(T, T)>),
+    Roots(Vec<T>),
+    /// Assumed index `i` to hold `C_i` when polynomial looks as follows:
+    /// `C_0 + C_1 * x + C_2 * x^2 + ... + C_n * x^n`
+    Coeff(Vec<T>),
+}
+
+/// A polynomial
+pub struct Polynomial<T> {
+    repr: PolynomialRepr<T>,
+}
+
+impl<T> Polynomial<T>
+where
+    T: Clone,
+{
+    /// Generate a polynomial from its coefficients `C_0`, `C_1`, ... `C_n`
+    /// when polynomial looks as follows:
+    /// `C_0 + C_1 * x + C_2 * x^2 + ... + C_n * x^n`
+    pub fn new_from_coeffs(coeffs: &[T]) -> Self {
+        Self {
+            repr: PolynomialRepr::Coeff(coeffs.to_vec()),
+        }
+    }
+}
+
+impl<T: Zero + Mul<Output = T> + Clone> Polynomial<T> {
+    /// Evaluates the polynomial at a point.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let polynomial = Polynomial::<u32>::new_from_coeffs(&[3, 2, 1]);
+    /// assert_eq!(polynomial.eval(0), 3);
+    /// assert_eq!(polynomial.eval(1), 6);
+    /// assert_eq!(polynomial.eval(10), 123);
+    /// ```
+    #[inline]
+    pub fn eval(&self, x: T) -> T {
+        match &self.repr {
+            PolynomialRepr::Points(_points) => unimplemented!(),
+            PolynomialRepr::Roots(_roots) => unimplemented!(),
+            PolynomialRepr::Coeff(coeffs) => {
+                let mut result: T = Zero::zero();
+                for n in coeffs.iter().rev() {
+                    result = n.clone() + result * x.clone();
+                }
+                result
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn eval_for_coeff_polynomial_repr() {
+        let polynomial = Polynomial::<u32>::new_from_coeffs(&[3, 2, 1]);
+        assert_eq!(polynomial.eval(0), 3);
+        assert_eq!(polynomial.eval(1), 6);
+        assert_eq!(polynomial.eval(10), 123);
+    }
+}
--- univariate-polynomial-iop-zerotest/src/lib.rs
+++ univariate-polynomial-iop-zerotest/src/lib.rs
@@ -1,16 +1,25 @@
 use ark_ff::fields::{Fp64, MontBackend, MontConfig};
 
 /// 41 = (2^3 x 5) + 1
-/// Hence, a 8th root of unity would exist in this
+/// Hence, a 8th root of unity would exist in this, since 2^3 is a divisor
 /// 3 is such root of unity. 3^8 = 6561. And 6561 % 41 = 1
 /// Building a field is given at: https://docs.rs/ark-ff/latest/ark_ff/fields/trait.Field.html
 #[derive(MontConfig)]
 #[modulus = "41"]
 #[generator = "2"]
-#[small_subgroup_base = "3"]
-#[small_subgroup_power = "8"]
 pub struct FqConfig;
 pub type Fq = Fp64<MontBackend<FqConfig, 1>>;
+
+pub enum PolynomialRepr<T> {
+    Points(Vec<(T, T)>),
+    Roots(Vec<T>),
+    Coeff(Vec<T>),
+}
+
+/// A polynomial
+pub struct Polynomial<T> {
+    repr: PolynomialRepr<T>,
+}
 
 #[cfg(test)]
 mod tests {
