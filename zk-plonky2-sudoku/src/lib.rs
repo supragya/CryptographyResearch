d05ff2ee9363e3cd238e81d5f2e243626bcd55af
Supragya Raj <supragyaraj@gmail.com>
2024-03-24 14:25:29 +0530

    some additions

--- zk-plonky2-sudoku/src/lib.rs
+++ zk-plonky2-sudoku/src/lib.rs
@@ -1,5 +1,9 @@
+use anyhow::Result;
 use plonky2::{
-    iop::witness::{PartialWitness, WitnessWrite},
+    iop::{
+        target::Target,
+        witness::{PartialWitness, WitnessWrite},
+    },
     plonk::{
         circuit_builder::CircuitBuilder,
         circuit_data::CircuitConfig,
@@ -8,37 +12,64 @@ use plonky2::{
 };
 use plonky2_field::types::Field;
 
-pub fn circuit_builder() {
+pub fn circuit_builder() -> Result<()> {
     const D: usize = 2;
     type C = PoseidonGoldilocksConfig;
     type F = <C as GenericConfig<D>>::F;
 
+    // 100 bits of security standard recursion config
     let cfg = CircuitConfig::standard_recursion_config();
     let mut builder = CircuitBuilder::<F, D>::new(cfg);
 
-    let initial = builder.add_virtual_target();
-    let mut cur_target = initial;
+    // Add 81 virtual targets (from 0 to 80), these will show up as
+    // Target::VirtualTarget { index: row*9 + col} in sudoku
+    let targets: Vec<Target> = (0..81).map(|_| builder.add_virtual_target()).collect();
 
-    for i in 1..101 {
-        let i_target = builder.constant(F::from_canonical_u32(i));
-        let n_plus_i = builder.add(initial, i_target);
-        cur_target = builder.mul(cur_target, n_plus_i);
-    }
+    let constants_0_to_9: Vec<Target> = (0..9).map(|i| builder.constant(F::from(i + 1))).collect();
 
-    builder.register_public_input(initial);
+    // Constraint: each row should have a values 1 through 9
+    // This is checked via a permuation check on constants 0-9
+    (0..9).for_each(|row| {
+        assert_permutation_circuit(builder, constants_0_to_9, targets[row * 9..(row + 1) * 9])
+    });
 
-    let mut pw = PartialWitness::new();
-    pw.set_target(initial, F::ONE);
+    // plonky2_waksman::permutation::assert_permutation_circuit(builder, a, b);
 
-    let data = builder.build::<C>();
-    let proof = data.prove(pw).unwrap();
+    // Add constraints: Each row should have
+    println!("{:#?}", targets);
 
-    println!(
-        "Factorial starting at {} is {}",
-        proof.public_inputs[0], proof.public_inputs[1]
-    );
+    Ok(())
+    // let initial_a = builder.add_virtual_target();
+    // let initial_b = builder.add_virtual_target();
 
-    data.verify(proof).unwrap()
+    // // println!("{:#?}", initial_b);
+
+    // let mut prev_target = initial_a;
+    // let mut cur_target = initial_b;
+    // for _ in 0..99 {
+    //     let temp = builder.add(prev_target, cur_target);
+    //     prev_target = cur_target;
+    //     cur_target = temp;
+    // }
+
+    // // Public inputs are the two initial values (provided below) and the result (which is generated).
+    // builder.register_public_input(initial_a);
+    // builder.register_public_input(initial_b);
+    // builder.register_public_input(cur_target);
+
+    // let mut pw = PartialWitness::new();
+    // pw.set_target(initial_a, F::ZERO);
+    // pw.set_target(initial_b, F::ONE);
+
+    // let data = builder.build::<C>();
+    // let proof = data.prove(pw)?;
+
+    // println!(
+    //     "100th Fibonacci number mod |F| (starting with {}, {}) is: {}",
+    //     proof.public_inputs[0], proof.public_inputs[1], proof.public_inputs[2]
+    // );
+
+    // data.verify(proof)
 }
 
 #[cfg(test)]
@@ -47,6 +78,6 @@ mod tests {
 
     #[test]
     fn ensure_facts() {
-        circuit_builder()
+        circuit_builder().unwrap()
     }
 }
