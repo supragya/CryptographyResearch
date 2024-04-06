a87d50d24ceefb71af0855fc3f3db738fb333712
Supragya Raj <supragyaraj@gmail.com>
2024-04-06 14:22:11 +0530

    fix: reconstruction

--- shamir-secret-sharing/src/lib.rs
+++ shamir-secret-sharing/src/lib.rs
@@ -12,33 +12,36 @@ mod tests {
         // Assume that we want to break secret into 4 "parts",
         // of which any 2 should be able to reconstruct the
         // original secret
-        let _parts = 4;
+        let parts = 4;
         let threshold = 2;
 
         // Hence, coefficients will be of form `secret + r1*x + r2*x^2...`
         // where `r` is random values from `(0, threshold)`
-        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(3091);
-        let coeffs: Vec<i64> = [secret]
+        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
+        let coeffs: Vec<f64> = [secret as f64]
             .into_iter()
-            .chain((0..threshold).map(|_| rng.next_u32() as i64))
+            .chain((0..threshold-1).map(|_| rng.next_u32() as f64))
             .collect();
+
+        println!("{:?}", coeffs);
 
         let polynomial = Polynomial::new_from_coeffs(&coeffs);
 
         // Ensure that we can get the secret back given we evaluate
         // at 0
-        assert_eq!(polynomial.eval(0), secret);
+        assert_eq!(polynomial.eval(0.0) as i64, secret);
 
         // Now evaulate at a few random points to make the secret
-        // let secret_parts: Vec<(i64, i64)> = (0..threshold)
-        //     .map(|_| {
-        //         let point_of_eval = rng.next_u32() as i64;
-        //         let eval = polynomial.eval(point_of_eval);
-        //         (point_of_eval, eval)
-        //     })
-        //     .collect();
+        let secret_parts: Vec<(f64, f64)> = (0..parts)
+            .map(|idx| {
+                let eval = polynomial.eval(idx as f64 + 1.0); // + 1.0 required as we do not want to generate at 0
+                (idx as f64 + 1.0, eval)
+            })
+            .collect();
 
         // Ensure reconstruction is possible
-        // let reconstructed_poly = Polynomial::new_from_evalutations()
+        let reconstructed_poly = Polynomial::new_from_evals(&[secret_parts[0], secret_parts[3]]);
+
+        assert!(reconstructed_poly.eval(0.0)-(secret as f64) < 0.0000001); // due to f64 inaccuracy
     }
 }
